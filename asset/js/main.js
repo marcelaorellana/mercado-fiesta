e8a4dbe904b4651491eb1ee86304068cdbb11275
marcelaorellana <marcelapaz.1985@gmail.com>
2017-06-10 19:51:57 -0400

    cosas

---
+++ asset/js/main.js
@@ -0,0 +1,18 @@
+document.getElementById("cumple").addEventListener("click", function(event){
+    event.preventDefault()
+});
+document.getElementById("asado").addEventListener("click", function(event){
+    event.preventDefault()
+});
+document.getElementById("matri").addEventListener("click", function(event){
+    event.preventDefault()
+});
+document.getElementById("fiesta").addEventListener("click", function(event){
+    event.preventDefault()
+});
+
+document.getElementById("planID").addEventListener("click", function(event){
+    var boton = document.getElementById("calen");
+    boton.classList.toggle('hiden');
+
+});
